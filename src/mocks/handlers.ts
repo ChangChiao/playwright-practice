99073c37daaa2483921c0956734570d7ca3cd8fc
JoeCT_Chang <JoeCT.Chang@moxa.com>
2023-05-13 19:21:51 +0800

    feat(test): add playwright

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -18,6 +18,7 @@ export const handlers = [
     // If authenticated, return a mocked user details
     return res(
       ctx.status(200),
+      ctx.cookie("auth-token", "63099c80-773d-4bca-b2ba-bea12c9fb44e"),
       ctx.json({
         username: "admin",
       })
